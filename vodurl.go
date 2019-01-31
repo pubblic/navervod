e0439059744b7613373943ac733117ac1ece4eef
elenchi <pubblic256@gmail.com>
2019-01-31 10:12:16 +0000

    vodurl

---
+++ vodurl.go
@@ -0,0 +1,21 @@
+package navervod
+
+import (
+	"net/url"
+)
+
+var baseurl, _ = url.Parse("https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/")
+
+func Playurl(vid, key string, q url.Values) string {
+	if q == nil {
+		q = make(url.Values, 1)
+	}
+	_, ok := q["key"]
+	if !ok {
+		q.Set("key", key)
+	}
+	u := *baseurl
+	u.Path += key
+	u.RawQuery = q.Encode()
+	return u.Encode()
+}
